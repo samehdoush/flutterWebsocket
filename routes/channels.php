c41f740ca15cd599731ad1e63cd62ed30a6be96c
sameh doush <samehdev95@gmail.com>
2023-02-13 20:38:13 +0300

    Update channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::routes();
-Broadcast::routes(['prefix' => 'api', 'middleware' => 'auth:sanctum']);
+
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
